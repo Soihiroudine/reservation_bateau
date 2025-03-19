136615460c8de70c5a23d0c752e4b137fad540bd
Soihiroudine <59316622+Soihiroudine@users.noreply.github.com>
2025-03-19 15:20:17 +0300

    Liaison des route avec le fichier principale

--- app.js
+++ app.js
@@ -11,14 +11,19 @@ executionDB("requetes.sql");
 
 // On appele les routes
 const routeAcceuil = require("./server/routes/routeAcceuil");
+const routeReservation = require("./server/routes/routeReservation");
+const routeUtilisateur = require("./server/routes/routeUtilisateur");
 
 // Acces au fichier du projet - css, image
 app.use(express.static("public"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Creation de routes
 app.use("/", routeAcceuil);
+app.use("/", routeReservation);
+app.use("/", routeUtilisateur);
 
 // On exporter l'application
 module.exports = app;
