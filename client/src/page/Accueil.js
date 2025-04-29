8067fdd1afb8c25bd695395e29cb4f80bb085da7
faoula <faoula.saindou18@gmail.com>
2025-04-29 10:22:44 +0200

    travail accueil

--- client/src/page/Accueil.js
+++ client/src/page/Accueil.js
@@ -2,6 +2,13 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import NavbarDafault from '../components/Navbar';
 import Header from '../components/Header';
+import bateau from "../assets/image acceuil/bateau.jpeg";
+import drapeau from "../assets/image acceuil/drapeau.png";
+import yacht from "../assets/image acceuil/yacht.jpeg";
+import tortue from "../assets/image acceuil/tortue.jpeg";
+import ilot from "../assets/image acceuil/ilot.jpg";
+import ylang from "../assets/image acceuil/ylang2.jpg";
+import Maki from "../assets/image acceuil/Maki.jpeg";
 
 const Accueil = () => {
 
@@ -19,14 +26,62 @@ const Accueil = () => {
 
     }, []);
 
+    
+
     return (
         <div>
             {/* <NavbarDafault /> */}
             <Header>
                 <NavbarDafault/>
             </Header>
-            <h1>Bienvenue sur mon site Web !</h1>
-            <p>{message}</p>
+
+    <div id="slider">
+        <img src={bateau} alt="La forêt de peupliers" id="slide"/>
+        <div id="precedent" onclick="ChangeSlide(-1)"></div>
+        <div id="suivant" onclick="ChangeSlide(1)"></div>
+
+        <img src={drapeau} alt="La forêt de peupliers" id="slide"/>
+        <div id="precedent" onclick="ChangeSlide(-1)"></div>
+        <div id="suivant" onclick="ChangeSlide(1)"></div>
+    </div>
+
+    <div id="slider">
+        <img src={yacht} alt="La forêt de peupliers" id="slide"/>
+        <div id="precedent" onclick="ChangeSlide(-1)"></div>
+        <div id="suivant" onclick="ChangeSlide(1)"></div>
+    </div>
+
+    <div id="slider">
+        <img src={tortue} alt="La forêt de peupliers" id="slide"/>
+        <div id="precedent" onclick="ChangeSlide(-1)"></div>
+        <div id="suivant" onclick="ChangeSlide(1)"></div>
+    </div>
+
+    <div id="slider">
+        <img src={ilot} alt="La forêt de peupliers" id="slide"/>
+        <div id="precedent" onclick="ChangeSlide(-1)"></div>
+        <div id="suivant" onclick="ChangeSlide(1)"></div>
+    </div>
+
+    <div id="slider">
+        <img src={ylang} alt="La forêt de peupliers" id="slide"/>
+        <div id="precedent" onclick="ChangeSlide(-1)"></div>
+        <div id="suivant" onclick="ChangeSlide(1)"></div>
+    </div>
+
+
+    <div id="slider">
+        <img src={Maki} alt="La forêt de peupliers" id="slide"/>
+        <div id="precedent" onclick="ChangeSlide(-1)"></div>
+        <div id="suivant" onclick="ChangeSlide(1)"></div>
+    </div>
+
+
+
+
+    <p>{message}</p>
+
+    
         </div>
     );
 }
